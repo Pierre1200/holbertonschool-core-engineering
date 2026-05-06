3578353535cca98bc912628f62535b1622f2f9c8
Pierre1200 <rouvellat.p@icloud.com>
2026-05-06 15:07:08 +0200

    Add uppercase.py function to convert strings to uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for char in str:
+        if ord("a") <= ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print(char, end="")
+    print("")
