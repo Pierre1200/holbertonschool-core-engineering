5e9890a32d6c2551df0fba24cb0447f809e8019d
Pierre1200 <rouvellat.p@icloud.com>
2026-05-07 11:23:26 +0200

    Fix variable name in print_matrix_integer function to correct indexing error

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for lignes in matrix:
         for i in range(len(lignes)):
             print("{:d}".format(lignes[i]), end="")
-            if i < len(ligne) - 1:
+            if i < len(lignes) - 1:
                 print(" ", end="")
         print()
