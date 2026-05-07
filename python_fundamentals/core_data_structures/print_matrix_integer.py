73acee46da5656732601e79e1b1be3cec58d4b92
Pierre1200 <rouvellat.p@icloud.com>
2026-05-07 11:21:01 +0200

    Fix syntax error in print_matrix_integer function by correcting print statement

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for lignes in matrix:
         for i in range(len(lignes)):
-            print("{:d}".format(lignes[i], end""))
+            print("{:d}".format(lignes[i]), end="")
             if i < len(ligne) - 1:
                 print(" ", end="")
         print()
