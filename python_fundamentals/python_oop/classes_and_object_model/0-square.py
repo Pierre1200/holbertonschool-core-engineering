24a38a8242b63e74b045a3c654cf8e68d2315884
Pierre1200 <rouvellat.p@icloud.com>
2026-05-12 10:28:26 +0200

    Update module docstring in 0-square.py for clarity and conciseness

--- python_fundamentals/python_oop/classes_and_object_model/0-square.py
+++ python_fundamentals/python_oop/classes_and_object_model/0-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """
-Module 0-square
-Ce module fournit une définition simple de la classe Square.
+Ce module définit une classe Square vide.
 """
 
 
 class Square:
     """
     Classe Square qui définit un carré.
-
-    Cette classe est actuellement vide pour illustrer la création 
-    de base d'une classe en Python.
     """
     pass
