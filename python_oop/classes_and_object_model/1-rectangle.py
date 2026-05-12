292c478974b8bffbb4bffde6d019e7b3d1dab27a
Pierre1200 <rouvellat.p@icloud.com>
2026-05-12 14:35:19 +0200

    Add 1-rectangle.py module with Rectangle class, width and height properties, and validation logic

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Module docstring """
+
+
+class Rectangle:
+    """ Class docstring """
+
+    def __init__(self, width=0, height=0):
+        """ Initialise le carré """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter : Récupère la taille """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter : Modifie la taille avec validation """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter : Récupère la hauteur """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter : Récupère la hauteur """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
