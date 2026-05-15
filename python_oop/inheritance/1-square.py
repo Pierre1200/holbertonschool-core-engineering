6c6c210e13fd57edf3007b0437afc9110c8cbac1
Pierre1200 <rouvellat.p@icloud.com>
2026-05-15 14:41:39 +0200

    Add Square class that inherits from Rectangle with area calculation

---
+++ python_oop/inheritance/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module BaseGeometry """
+Rectangle = __import__('1-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """classe square"""
+    def __init__(self, size):
+        """Init de la forme"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """calcul de l'aire"""
+        return self.__size * self.__size
