a2fb961c2b017b1fdf3356d88fe6db97f1f8ad6d
Pierre1200 <rouvellat.p@icloud.com>
2026-05-15 11:59:17 +0200

    Fix integer_validator docstring and error message for clarity

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -9,8 +9,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Valide si value est un entier strictement positif integer_validation """
+        """ Valide si value est un entier strictement positif """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(value))
+            raise ValueError("{} must be greater than 0".format(name))
