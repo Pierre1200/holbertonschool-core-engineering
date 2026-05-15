a68f699823b89b5f3e6bb81b89ca5874efc8d51c
Pierre1200 <rouvellat.p@icloud.com>
2026-05-15 14:22:22 +0200

    Remove unnecessary blank lines and comment in Rectangle class for clarity

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """ Module BaseGeometry """
-
-
 BaseGeometry = __import__('0-base_geometry').BaseGeometry
-    """import de base_geometrie"""
+
 
 class Rectangle(BaseGeometry):
     """Classe rectangle"""
