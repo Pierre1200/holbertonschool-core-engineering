9182e6d1809851f0edc5e082a28db7c97b6ca6cc
Pierre1200 <rouvellat.p@icloud.com>
2026-05-26 09:51:17 +0200

    Add docstrings to read_file module and function for clarity

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Module de lecture"""
+
 
 def read_file(filename=""):
+    """Fonction de lecture"""
     with open("", "r", encoding="utf-8") as f:
         content = f.read()
         print(content)
