3a706fbcad5819efb5a75f5aa8eaec3bdc24f65c
Pierre1200 <rouvellat.p@icloud.com>
2026-05-26 10:15:23 +0200

    Add append_write function for appending text to a file

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module d'ajout"""
+
+
+def append_write(filename="", text=""):
+    """Fonction d'ajout"""
+    with open(filename, "a", encoding="utf-8") as f:
+        f.append(text)
