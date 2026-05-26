fd64811e179522c35f0a3424bc2bbd496d2cb870
Pierre1200 <rouvellat.p@icloud.com>
2026-05-26 10:08:30 +0200

    Fix variable name in write_file function for clarity

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """Fonction d'écriture"""
-    with open(filename, "w", encoding="utf-8") as filename:
-        filename.write(text)
+    with open(filename, "w", encoding="utf-8") as f:
+       return f.write(text)
