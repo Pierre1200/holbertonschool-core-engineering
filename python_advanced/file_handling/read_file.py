f9b9cb1ed2f418c646aec5238452465d225ec71c
Pierre1200 <rouvellat.p@icloud.com>
2026-05-26 09:57:12 +0200

    Fix read_file function to use the provided filename for file opening

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Fonction de lecture"""
-    with open("", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
-        print(content)
+        print(content, end="")
