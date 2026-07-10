cdb3895cd60596fc00d044a7aabb6169967a0aca
Pierre1200 <rouvellat.p@icloud.com>
2026-07-10 14:02:56 +0200

    Add broadcast server implementation for WebSocket communication

---
+++ python_advanced/websockets/broadcast_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+import websockets
+
+connected_clients = set()
+
+
+async def connection_handler(websocket):
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in connected_clients:
+                try:
+                    await client.send("B:" + message)
+                except websockets.exceptions.ConnectionClosed:
+                    pass
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        connected_clients.discard(websocket)
+
+
+async def main():
+    async with websockets.serve(connection_handler, "localhost", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
